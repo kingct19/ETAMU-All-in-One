ae7a71a2dfed1992b959ffe118e2aedb4a22db96
Oluwaseun Arogundade <oarogundade@leomail.tamuc.edu>
2025-03-28 16:55:16 -0500

    chore: add app_launcher utility for external app launching with fallback

---
+++ etamu_all_in_one/lib/utils/app_launcher.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+import 'package:android_intent_plus/android_intent_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+Future<void> launchAppOrStore({
+  required String androidPackageName,
+  required String iosAppStoreUrl,
+}) async {
+  if (Platform.isAndroid) {
+    final intent = AndroidIntent(
+      action: 'action_view',
+      package: androidPackageName,
+    );
+    try {
+      await intent.launch();
+    } catch (e) {
+      final Uri playStoreUri =
+          Uri.parse("https://play.google.com/store/apps/details?id=$androidPackageName");
+      await launchUrl(playStoreUri, mode: LaunchMode.externalApplication);
+    }
+  } else if (Platform.isIOS) {
+    final Uri appStoreUri = Uri.parse(iosAppStoreUrl);
+    await launchUrl(appStoreUri, mode: LaunchMode.externalApplication);
+  }
+}
